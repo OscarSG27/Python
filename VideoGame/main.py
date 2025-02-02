17e21cb5566638dc03ba1145b3e55848055bbc7b
OscarSG27 <134946261+OscarSG27@users.noreply.github.com>
2025-02-02 18:09:58 +0100

    Delete VideoGame/main.py

--- VideoGame/main.py
+++
@@ -1,6 +0,0 @@
-from Clases import Juego
-
-# Si el archivo principal se llama main, ejecuta el método ejecutar del objeto Juego()
-if __name__ == "__main__":
-    juego = Juego()
-    juego.ejecutar()
