8115454b6ab8a0c7705a8c129a1b5151dbf8b5f0
OscarSG27 <134946261+OscarSG27@users.noreply.github.com>
2025-02-02 16:17:35 +0100

    Add files via upload
    This is my first VideoGame. For thi, I used the PyGame library.

---
+++ VideoGame/main.py
@@ -0,0 +1,6 @@
+from Clases import Juego
+
+# Si el archivo principal se llama main, ejecuta el método ejecutar del objeto Juego()
+if __name__ == "__main__":
+    juego = Juego()
+    juego.ejecutar()
